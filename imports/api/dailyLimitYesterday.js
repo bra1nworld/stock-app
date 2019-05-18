7a210c3572dc4a940cac3057148c91338eade16e
dinghongyu <dinghongyu@huohua.cn>
2019-05-18 18:07:43 +0800

    build test

--- imports/api/dailyLimitYesterday.js
+++ imports/api/dailyLimitYesterday.js
@@ -7,12 +7,12 @@ const Settings = {
     firstSearchUrl: `http://1.push2his.eastmoney.com/api/qt/stock/trends2/get?fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13&fields2=f51,f52,f53,f54,f55,f56,f57,f58&ut=fa5fd1943c7b386f172d6893dbfba10b&iscr=0&ndays=1&secid=90.BK0815&cb=jQuery183010876406714142406_1556033065016&_=1556033065316`
 };
 const absPath = path.resolve("./");
+console.log(absPath);
+
 console.log("absPath.indexOf(root)");
 console.log(absPath.indexOf("root"));
 
 const startDateHour = absPath.indexOf("root") > 0 ? 1 : 9;
-const backupDataPath =
-    absPath.slice(0, absPath.indexOf(".meteor")) + "testStock.txt";
 
 export const DailyLimit = new Mongo.Collection("dailyLimit");
 
@@ -20,83 +20,47 @@ if (Meteor.isServer) {
     Meteor.publish("dailyLimit", function dailyLimitPublication() {
         return DailyLimit.find({});
     });
-    const allDataLen = DailyLimit.find({}).count();
 
-    if (allDataLen === 0) {
-        fs.readFile(
-            backupDataPath,
-            //must wrapped by bindEnvironment when use native callback function
-            Meteor.bindEnvironment((err, result) => {
-                if (err || !result) {
-                    console.log(err);
-                    return;
-                }
-                const oldData = JSON.parse(result.toString());
+    (async () => {
+        let browser = await puppeteer.launch({
+            timeout: 30000,
+            ignoreHTTPSErrors: true,
+            devtools: false,
+            headless: true,
+            args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        });
 
-                for (let item of oldData) {
-                    DailyLimit.insert(item);
-                }
-            })
-        );
-    } else {
-        (async () => {
-            let browser = await puppeteer.launch({
-                timeout: 30000,
-                ignoreHTTPSErrors: true,
-                devtools: false,
-                headless: true,
-                args: ["--no-sandbox", "--disable-setuid-sandbox"]
-            });
+        const searchPage = await browser.newPage();
 
-            const searchPage = await browser.newPage();
+        searchPage.on("response", async res => {
+            const resText = await res.text();
+            const resultJsonStr = resText.match(/[^\(\)]+(?=\))/g)[0];
+            const resJson = JSON.parse(resultJsonStr);
+            updateDailyLimitYesterday(resJson.data);
 
-            searchPage.on("response", async res => {
-                const resText = await res.text();
-                const resultJsonStr = resText.match(/[^\(\)]+(?=\))/g)[0];
-                const resJson = JSON.parse(resultJsonStr);
-                updateDailyLimitYesterday(resJson.data);
+            const now = Date.now();
+            let todayStart = new Date(now).setHours(startDateHour, 25, 0, 0);
+            let todayEnd = new Date(now).setHours(startDateHour + 6, 00, 0, 0);
+            let todayUpdateLastTime = new Date(now).setHours(
+                startDateHour + 6,
+                10,
+                0,
+                0
+            );
 
-                const now = Date.now();
-                let todayStart = new Date(now).setHours(
-                    startDateHour,
-                    25,
-                    0,
-                    0
-                );
-                let todayEnd = new Date(now).setHours(
-                    startDateHour + 6,
-                    00,
-                    0,
-                    0
-                );
-                let todayUpdateLastTime = new Date(now).setHours(
-                    startDateHour + 6,
-                    10,
-                    0,
-                    0
-                );
+            if (now > todayStart && now < todayEnd) {
+                setTimeout(() => {
+                    goPage(searchPage, `${Settings.firstSearchUrl}`);
+                }, 30000);
+            } else {
+                setTimeout(() => {
+                    goPage(searchPage, `${Settings.firstSearchUrl}`);
+                }, 5 * 60000);
+            }
+        });
 
-                if (now > todayStart && now < todayEnd) {
-                    setTimeout(() => {
-                        goPage(searchPage, `${Settings.firstSearchUrl}`);
-                    }, 30000);
-                } else {
-                    if (now > todayEnd && now < todayUpdateLastTime) {
-                        const dataArr = DailyLimit.find({}).fetch();
-                        dataArr.forEach(data => {
-                            delete data._id;
-                        });
-                        updateBackupData(dataArr);
-                    }
-                    setTimeout(() => {
-                        goPage(searchPage, `${Settings.firstSearchUrl}`);
-                    }, 5 * 60000);
-                }
-            });
-
-            await searchPage.goto(`${Settings.firstSearchUrl}`);
-        })();
-    }
+        await searchPage.goto(`${Settings.firstSearchUrl}`);
+    })();
 
     async function goPage(searchPage, url) {
         try {
@@ -108,18 +72,6 @@ if (Meteor.isServer) {
                 }, 30000);
             }
         }
-    }
-    function updateBackupData(arr) {
-        fs.writeFile(backupDataPath, JSON.stringify(arr), function(
-            err,
-            result
-        ) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log(result);
-        });
     }
 
     function updateDailyLimitYesterday(data) {
